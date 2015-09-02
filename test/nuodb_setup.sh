d1c9031e6a1b90185682d2126905b44e913619d4
Mukund <bal.mukund4u@gmail.com>
2015-09-02 10:13:53 +0530

    Update nuodb_setup.sh

--- test/nuodb_setup.sh
+++ test/nuodb_setup.sh
@@ -9,11 +9,11 @@ sudo dpkg -i /var/tmp/nuodb.deb
 /opt/nuodb/etc/nuorestsvc stop
 /opt/nuodb/etc/nuowebconsole stop
 
-cp ./test/default.properties /opt/nuodb/etc/.
+sudo cp ./test/default.properties /opt/nuodb/etc/.
 
-/opt/nuodb/etc/nuoagent start
-/opt/nuodb/etc/nuorestsvc start
-/opt/nuodb/etc/nuowebconsole start
+sudo /opt/nuodb/etc/nuoagent start
+sudo /opt/nuodb/etc/nuorestsvc start
+sudo /opt/nuodb/etc/nuowebconsole start
 
 ${NUODB_HOME}/bin/nuodbmgr --broker localhost --password bird --command "start process sm host localhost database test archive /var/tmp/nuodb initialize true"
 ${NUODB_HOME}/bin/nuodbmgr --broker localhost --password bird --command "start process te host localhost database test options '--dba-user ${NUODB_USERNAME} --dba-password ${NUODB_PASSWORD}'"
